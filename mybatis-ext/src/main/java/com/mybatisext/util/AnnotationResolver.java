dadce83b19210de1f96d9a0c9e30275618d444eb
Faputa <2238932729@qq.com>
2023-11-24 13:37:35 +0800

    取消检查fixedClass

--- mybatis-ext/src/main/java/com/mybatisext/util/AnnotationResolver.java
+++ mybatis-ext/src/main/java/com/mybatisext/util/AnnotationResolver.java
@@ -2,8 +2,6 @@ package com.mybatisext.util;
 
 import java.lang.annotation.Annotation;
 
-import javax.annotation.Nullable;
-
 public class AnnotationResolver {
 
     public static boolean hasAnnotation(Class<?> clazz, Class<? extends Annotation> annotation) {
@@ -21,26 +19,21 @@ public class AnnotationResolver {
         return false;
     }
 
-    public static <T extends Annotation> T findAnnotation(Class<?> clazz, Class<T> annotationClass,
-            @Nullable Class<?> fixedClass) {
-        // 检查固定类是否在继承路径上
-        if (fixedClass != null && !clazz.isAssignableFrom(fixedClass) && !fixedClass.isAssignableFrom(clazz)) {
-            return null;
-        }
+    public static <T extends Annotation> T findAnnotation(Class<?> clazz, Class<T> annotationClass) {
         T annotation = clazz.getAnnotation(annotationClass);
         if (annotation != null) {
             return annotation;
         }
         Class<?> superClass = clazz.getSuperclass();
         if (superClass != null) {
-            T superClassAnnotation = findAnnotation(superClass, annotationClass, fixedClass);
+            T superClassAnnotation = findAnnotation(superClass, annotationClass);
             if (superClassAnnotation != null) {
                 return superClassAnnotation;
             }
         }
         Class<?>[] interfaces = clazz.getInterfaces();
         for (Class<?> iface : interfaces) {
-            T interfaceAnnotation = findAnnotation(iface, annotationClass, fixedClass);
+            T interfaceAnnotation = findAnnotation(iface, annotationClass);
             if (interfaceAnnotation != null) {
                 return interfaceAnnotation;
             }
