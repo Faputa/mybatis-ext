85aff903f15b67682333be68dc4fa1b936df2b1b
Faputa <2238932729@qq.com>
2023-11-24 14:12:09 +0800

    compile同时编译测试类

--- mybatis-plus-test/src/test/java/com/mybatisext/test/plus/DemoMapper.java
+++ mybatis-plus-test/src/test/java/com/mybatisext/test/plus/DemoMapper.java
@@ -2,10 +2,7 @@ package com.mybatisext.test.plus;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.mybatisext.annotation.MapTable;
-
 @Mapper
-@MapTable(Demo.class)
 public interface DemoMapper extends DemoMapper0 {
 
     int countDidaTask();
