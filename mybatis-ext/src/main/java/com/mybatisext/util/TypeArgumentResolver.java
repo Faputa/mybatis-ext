b428008330c77a0781391b73319fb3b38a234049
Faputa <2238932729@qq.com>
2023-11-26 13:43:07 +0800

    重构TypeArgumentResolver并增加单元测试

--- mybatis-ext/src/main/java/com/mybatisext/util/TypeArgumentResolver.java
+++ mybatis-ext/src/main/java/com/mybatisext/util/TypeArgumentResolver.java
@@ -7,63 +7,69 @@ import java.util.Objects;
 
 import javax.annotation.Nullable;
 
+/**
+ * 解析泛型类型参数的实用工具类。
+ */
 public class TypeArgumentResolver {
 
-    public static Class<?> findClass(Class<?> clazz, Class<?> search, int index) {
-        return findClass(clazz, search, index, null);
+    /**
+     * 解析泛型类在指定索引处的类型参数。
+     *
+     * @param sourceType 起始解析的原始类。
+     * @param targetType 目标泛型类。
+     * @param index      要解析的类型参数的索引。
+     * @return 解析得到的类型参数的Class对象，如果未找到则为null。
+     */
+    public static Class<?> resolveTypeArgument(Class<?> sourceType, Class<?> targetType, int index) {
+        return resolveTypeArgument(sourceType, null, targetType, index);
     }
 
-    private static Class<?> findClass(Class<?> clazz, Class<?> search, int index, @Nullable Type[] clazzTypeArguments) {
+    private static Class<?> resolveTypeArgument(Class<?> sourceType, @Nullable Type[] sourceTypeArguments, Class<?> targetType, int index) {
         // 找到了
-        if (clazz == search) {
-            if (clazzTypeArguments != null && clazzTypeArguments[index] instanceof Class) {
-                return (Class<?>) clazzTypeArguments[index];
+        if (sourceType == targetType && sourceTypeArguments != null && sourceTypeArguments[index] instanceof Class) {
+            return (Class<?>) sourceTypeArguments[index];
+        }
+        // 检查接口
+        for (Type interfaceType : sourceType.getGenericInterfaces()) {
+            Class<?> resolvedClass = analyzeGenericType(interfaceType, sourceType, sourceTypeArguments, targetType, index);
+            if (resolvedClass != null) {
+                return resolvedClass;
             }
         }
-        // 检查基接口
-        Type[] genericInterfaces = clazz.getGenericInterfaces();
-        for (Type type : genericInterfaces) {
-            if (type instanceof ParameterizedType) {
-                // 泛型基接口
-                ParameterizedType parameterizedType = (ParameterizedType) type;
-                Type rawType = parameterizedType.getRawType();
-                if (rawType instanceof Class) {
-                    Type[] typeArguments = parameterizedType.getActualTypeArguments();
-                    adjustTypeArguments(clazz, clazzTypeArguments, typeArguments);
-                    Class<?> entityClass = findClass((Class<?>) rawType, search, index, typeArguments);
-                    if (entityClass != null) {
-                        return entityClass;
-                    }
-                }
-            } else if (type instanceof Class) {
-                // 其他基接口
-                Class<?> entityClass = findClass((Class<?>) type, search, index);
-                if (entityClass != null) {
-                    return entityClass;
-                }
-            }
-        }
-        // 检查基类
-        Class<?> superclass = clazz.getSuperclass();
-        if (superclass == null || superclass == Object.class) {
-            return null;
-        }
-        Type[] typeArguments = superclass.getTypeParameters();
-        adjustTypeArguments(clazz, clazzTypeArguments, typeArguments);
-        return findClass(superclass, search, index, typeArguments);
+        // 检查超类
+        return analyzeGenericType(sourceType.getGenericSuperclass(), sourceType, sourceTypeArguments, targetType, index);
     }
 
-    private static void adjustTypeArguments(Class<?> clazz, @Nullable Type[] clazzTypeArguments, Type[] typeArguments) {
-        if (clazzTypeArguments == null) {
-            return;
+    private static Class<?> analyzeGenericType(Type genericType, Class<?> sourceType, @Nullable Type[] sourceTypeArguments, Class<?> targetType, int index) {
+        if (genericType instanceof ParameterizedType) {
+            return analyzeParameterizedType((ParameterizedType) genericType, sourceType, sourceTypeArguments, targetType, index);
         }
+        if (genericType instanceof Class) {
+            return resolveTypeArgument((Class<?>) genericType, targetType, index);
+        }
+        return null;
+    }
+
+    private static Class<?> analyzeParameterizedType(ParameterizedType parameterizedType, Class<?> sourceType, @Nullable Type[] sourceTypeArguments, Class<?> targetType, int index) {
+        Type rawType = parameterizedType.getRawType();
+        if (rawType instanceof Class) {
+            Type[] typeArguments = parameterizedType.getActualTypeArguments();
+            if (sourceTypeArguments != null) {
+                adjustTypeArguments(sourceType, sourceTypeArguments, typeArguments);
+            }
+            return resolveTypeArgument((Class<?>) rawType, typeArguments, targetType, index);
+        }
+        return null;
+    }
+
+    private static void adjustTypeArguments(Class<?> sourceType, Type[] sourceTypeArguments, Type[] typeArguments) {
         for (int i = 0; i < typeArguments.length; i++) {
             if (typeArguments[i] instanceof TypeVariable) {
                 TypeVariable<?> typeVariable = (TypeVariable<?>) typeArguments[i];
-                TypeVariable<?>[] typeParameters = clazz.getTypeParameters();
+                TypeVariable<?>[] typeParameters = sourceType.getTypeParameters();
                 for (int j = 0; j < typeParameters.length; j++) {
                     if (Objects.equals(typeVariable.getName(), typeParameters[j].getName())) {
-                        typeArguments[i] = clazzTypeArguments[j];
+                        typeArguments[i] = sourceTypeArguments[j];
                         break;
                     }
                 }
