5ad556a917930aaf41078fe2decabaa1c6b35a77
Faputa <2238932729@qq.com>
2023-11-27 23:52:07 +0800

    连表查询增加fetchType设置

--- mybatis-ext/src/main/java/io/github/mybatisext/annotation/JoinColumns.java
+++ mybatis-ext/src/main/java/io/github/mybatisext/annotation/JoinColumns.java
@@ -6,10 +6,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.apache.ibatis.mapping.FetchType;
+
 @Inherited
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JoinColumns {
 
+    /** 关联列 */
     JoinColumn[] value();
+
+    /** 获取类型，默认连表查询 */
+    FetchType fetchType() default FetchType.DEFAULT;
 }
