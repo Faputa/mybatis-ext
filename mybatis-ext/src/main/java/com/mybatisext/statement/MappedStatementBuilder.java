059367fc86e5027f0faa9148c56fb113ffc96018
Faputa <2238932729@qq.com>
2023-11-27 10:03:32 +0800

    springboot增加是否启用配置，默认开启

--- mybatis-ext/src/main/java/com/mybatisext/statement/MappedStatementBuilder.java
+++ mybatis-ext/src/main/java/com/mybatisext/statement/MappedStatementBuilder.java
@@ -15,29 +15,24 @@ import org.apache.ibatis.session.Configuration;
 
 public class MappedStatementBuilder {
 
-    private final Configuration configuration;
+    private final Configuration originConfiguration;
 
-    public MappedStatementBuilder(Configuration configuration) {
-        this.configuration = configuration;
+    public MappedStatementBuilder(Configuration originConfiguration) {
+        this.originConfiguration = originConfiguration;
     }
 
-    public MappedStatement build(String id, String methodName, List<Method> methods, Class<?> returnType,
-            Class<?> tableType) {
+    public MappedStatement build(String id, String methodName, List<Method> methods, Class<?> returnType, Class<?> tableType) {
         if (!"countDidaTask2".equals(methodName)) {
             return null;
         }
         // TODO
         // 考虑自己实现SqlSource
         XMLLanguageDriver xmlLanguageDriver = new XMLLanguageDriver();
-        SqlSource sqlSource = xmlLanguageDriver.createSqlSource(configuration,
-                "<script>select count(*) from org</script>",
-                Object.class);
+        SqlSource sqlSource = xmlLanguageDriver.createSqlSource(originConfiguration, "<script>select count(*) from org</script>", Object.class);
         List<ResultMap> resultMaps = new ArrayList<>();
-        ResultMap inlineResultMap = new ResultMap.Builder(configuration, id + "-Inline", Integer.class,
-                new ArrayList<>(), null)
-                .build();
+        ResultMap inlineResultMap = new ResultMap.Builder(originConfiguration, id + "-Inline", Integer.class, new ArrayList<>(), null).build();
         resultMaps.add(inlineResultMap);
-        Builder builder = new MappedStatement.Builder(configuration, id, sqlSource, SqlCommandType.SELECT);
+        Builder builder = new MappedStatement.Builder(originConfiguration, id, sqlSource, SqlCommandType.SELECT);
         return builder.resultMaps(resultMaps).resultSetType(ResultSetType.DEFAULT).build();
     }
 }
