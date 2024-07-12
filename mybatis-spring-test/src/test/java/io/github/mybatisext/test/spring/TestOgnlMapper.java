c9464fd3bd2efd748a52141da78a35afe714a47b
Faputa <2238932729@qq.com>
2024-07-12 14:36:22 +0800

    考虑支持级联新增修改删除

---
+++ mybatis-spring-test/src/test/java/io/github/mybatisext/test/spring/TestOgnlMapper.java
@@ -0,0 +1,14 @@
+package io.github.mybatisext.test.spring;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TestOgnlMapper {
+    List<Object> testOgnlInForeach(Object value);
+
+    List<Object> testVaragsToList(Object a, Object b);
+
+	List<Object> testOgnlCallMapperStatement();
+}
