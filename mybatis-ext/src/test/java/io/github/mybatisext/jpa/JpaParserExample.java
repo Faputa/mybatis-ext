f8ca99f05020820ec219b8c9e0693fa09b82c4e6
Faputa <2238932729@qq.com>
2024-08-12 16:35:57 +0800

    jpa解析器支持默认使用方法参数名作为条件

--- mybatis-ext/src/test/java/io/github/mybatisext/jpa/JpaParserExample.java
+++ mybatis-ext/src/test/java/io/github/mybatisext/jpa/JpaParserExample.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.annotations.Param;
 
 public interface JpaParserExample {
 
-    void select();
+    void select(@Param("tableId") String tableId, @Param("userId") String userId);
 
     void getDistinctTop10ByUserId$AndTableIdAndRowPrivilegeFieldsDotFieldInXyz$OrderByCreateTime(@Param("xyz") String s);
 }
