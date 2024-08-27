0bb3e888e4aca3b4cbf1a3031026611c54a9174e
Faputa <2238932729@qq.com>
2024-08-28 01:47:00 +0800

    使用@OnlyById表示只用元数据参数的主键作为条件

--- mybatis-ext/src/main/java/io/github/mybatisext/mapper/BaseMapper.java
+++ mybatis-ext/src/main/java/io/github/mybatisext/mapper/BaseMapper.java
@@ -2,21 +2,23 @@ package io.github.mybatisext.mapper;
 
 import java.util.List;
 
+import io.github.mybatisext.annotation.OnlyById;
+
 public interface BaseMapper<T> extends ExtMapper<T> {
 
     int save(T entity);
 
     int saveBatch(List<T> list);
 
-    int update(T entity);
+    int update(@OnlyById T entity);
 
-    int updateIgnoreNull(T entity);
+    int updateIgnoreNull(@OnlyById T entity);
 
-    int delete(T query);
+    int delete(@OnlyById T query);
 
-    int deleteBatch(T query);
+    int deleteBatch(@OnlyById T query);
 
-    T get(T query);
+    T get(@OnlyById T query);
 
     List<T> list(T query);
 
