6058d96b396b92841902e926ea84ec794a19f6e1
Faputa <2238932729@qq.com>
2024-12-30 17:44:26 +0800

    重构Condition

---
+++ mybatis-ext/src/main/java/io/github/mybatisext/jpa/CompareOperator.java
@@ -0,0 +1,32 @@
+package io.github.mybatisext.jpa;
+
+public enum CompareOperator {
+
+    Equals,
+
+    LessThan,
+
+    LessThanEqual,
+
+    GreaterThan,
+
+    GreaterThanEqual,
+
+    Like,
+
+    StartWith,
+
+    EndWith,
+
+    Between,
+
+    In,
+
+    IsNull,
+
+    IsNotNull,
+
+    IsTrue,
+
+    IsFalse,
+}
