aba5f28c7207c087aa28063d4f7a0c4d93cb0e11
Faputa <2238932729@qq.com>
2025-01-13 18:37:30 +0800

    定义FilterSpec并接入属性元数据

---
+++ mybatis-ext/src/main/java/io/github/mybatisext/annotation/FilterSpec.java
@@ -0,0 +1,26 @@
+package io.github.mybatisext.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.github.mybatisext.jpa.CompareOperator;
+import io.github.mybatisext.jpa.LogicalOperator;
+
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FilterSpec {
+
+    IfTest test() default IfTest.NotNull;
+
+    CompareOperator operator() default CompareOperator.Equals;
+
+    LogicalOperator logicalOperator() default LogicalOperator.AND;
+
+    String testTemplate() default "";
+
+    String exprTemplate() default "";
+
+    String secondVariable() default "";
+}
---
+++ mybatis-ext/src/main/java/io/github/mybatisext/metadata/FilterSpecInfo.java
@@ -0,0 +1,82 @@
+package io.github.mybatisext.metadata;
+
+import java.util.Objects;
+
+import io.github.mybatisext.annotation.IfTest;
+import io.github.mybatisext.jpa.CompareOperator;
+import io.github.mybatisext.jpa.LogicalOperator;
+
+public class FilterSpecInfo {
+
+    private IfTest test;
+    private CompareOperator operator;
+    private LogicalOperator logicalOperator;
+    private String testTemplate;
+    private String exprTemplate;
+    private String secondVariable;
+
+    public IfTest getTest() {
+        return test;
+    }
+
+    public void setTest(IfTest test) {
+        this.test = test;
+    }
+
+    public CompareOperator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(CompareOperator operator) {
+        this.operator = operator;
+    }
+
+    public LogicalOperator getLogicalOperator() {
+        return logicalOperator;
+    }
+
+    public void setLogicalOperator(LogicalOperator logicalOperator) {
+        this.logicalOperator = logicalOperator;
+    }
+
+    public String getTestTemplate() {
+        return testTemplate;
+    }
+
+    public void setTestTemplate(String testTemplate) {
+        this.testTemplate = testTemplate;
+    }
+
+    public String getExprTemplate() {
+        return exprTemplate;
+    }
+
+    public void setExprTemplate(String exprTemplate) {
+        this.exprTemplate = exprTemplate;
+    }
+
+    public String getSecondVariable() {
+        return secondVariable;
+    }
+
+    public void setSecondVariable(String secondVariable) {
+        this.secondVariable = secondVariable;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FilterSpecInfo that = (FilterSpecInfo) o;
+        return test == that.test && operator == that.operator && logicalOperator == that.logicalOperator && Objects.equals(testTemplate, that.testTemplate) && Objects.equals(exprTemplate, that.exprTemplate) && Objects.equals(secondVariable, that.secondVariable);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(test, operator, logicalOperator, testTemplate, exprTemplate, secondVariable);
+    }
+}
