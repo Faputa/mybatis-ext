3367d3aa738c0fb27a7637074c0e00da580d29db
Faputa <2238932729@qq.com>
2025-01-13 14:11:00 +0800

    整理Java导入顺序

--- mybatis-ext/src/main/java/io/github/mybatisext/dialect/PostgreSqlDialect.java
+++ mybatis-ext/src/main/java/io/github/mybatisext/dialect/PostgreSqlDialect.java
@@ -1,9 +1,9 @@
 package io.github.mybatisext.dialect;
 
-import io.github.mybatisext.jpa.Limit;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import io.github.mybatisext.jpa.Limit;
 
 public class PostgreSqlDialect extends MySqlDialect {
 
