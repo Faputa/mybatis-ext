1ff0f91ae169f71aa5a24c85cf344647aaa39446
Faputa <2238932729@qq.com>
2025-01-19 22:51:46 +0800

    正确实现对mapper方法的校验

--- mybatis-ext-spring/src/main/java/io/github/mybatisext/spring/MybatisExtBeanPostProcessor.java
+++ mybatis-ext-spring/src/main/java/io/github/mybatisext/spring/MybatisExtBeanPostProcessor.java
@@ -1,29 +1,23 @@
-package io.github.mybatisext.spring.boot;
+package io.github.mybatisext.spring;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.stereotype.Component;
 
 import io.github.mybatisext.adapter.ExtContext;
-import io.github.mybatisext.dialect.DialectSelector;
-import io.github.mybatisext.spring.ExtSqlSessionFactoryBean;
 
-@Component
 public class MybatisExtBeanPostProcessor implements BeanPostProcessor {
 
-    @Autowired
-    private MybatisExtProperties properties;
-    @Autowired
-    private DialectSelector dialectSelector;
+    private ExtContext extContext = new ExtContext();
+
+    public void setExtContext(ExtContext extContext) {
+        this.extContext = extContext;
+    }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof SqlSessionFactory) {
             ExtSqlSessionFactoryBean extSqlSessionFactoryBean = new ExtSqlSessionFactoryBean();
-            ExtContext extContext = properties.toExtContext();
-            extContext.setDialectSelector(dialectSelector);
             extSqlSessionFactoryBean.setExtContext(extContext);
             return extSqlSessionFactoryBean.buildSqlSessionFactory((SqlSessionFactory) bean);
         }
