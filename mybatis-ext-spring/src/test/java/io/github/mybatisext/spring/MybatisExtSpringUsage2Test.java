6c3b1508fccc2acd81cb22ae820826fa9a743fe4
Faputa <2238932729@qq.com>
2025-01-20 17:31:12 +0800

    增加用法测试

---
+++ mybatis-ext-spring/src/test/java/io/github/mybatisext/spring/MybatisExtSpringUsage2Test.java
@@ -0,0 +1,41 @@
+package io.github.mybatisext.spring;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import io.github.mybatisext.test.SysDept;
+import io.github.mybatisext.test.SysDeptMapper;
+import io.github.mybatisext.test.SysMenu;
+import io.github.mybatisext.test.SysMenuMapper;
+import io.github.mybatisext.test.SysPost;
+import io.github.mybatisext.test.SysPostMapper;
+import io.github.mybatisext.test.SysRole;
+import io.github.mybatisext.test.SysRoleMapper;
+import io.github.mybatisext.test.SysUser;
+import io.github.mybatisext.test.SysUserMapper;
+
+public class MybatisExtSpringUsage2Test {
+
+	@Test
+	public void test() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContextUsage2.xml");
+		SysDeptMapper sysDeptMapper = context.getBean(SysDeptMapper.class);
+		SysMenuMapper sysMenuMapper = context.getBean(SysMenuMapper.class);
+		SysPostMapper sysPostMapper = context.getBean(SysPostMapper.class);
+		SysRoleMapper sysRoleMapper = context.getBean(SysRoleMapper.class);
+		SysUserMapper sysUserMapper = context.getBean(SysUserMapper.class);
+		List<SysDept> depts = sysDeptMapper.list(new SysDept());
+		List<SysMenu> menus = sysMenuMapper.list(new SysMenu());
+		List<SysPost> posts = sysPostMapper.list(new SysPost());
+		List<SysRole> roles = sysRoleMapper.list(new SysRole());
+		List<SysUser> users = sysUserMapper.list(new SysUser());
+		System.out.println("########" + depts);
+		System.out.println("########" + menus);
+		System.out.println("########" + posts);
+		System.out.println("########" + roles);
+		System.out.println("########" + users);
+		context.close();
+	}
+}
