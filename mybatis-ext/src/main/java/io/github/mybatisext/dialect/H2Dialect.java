4d4c15ecc6544b9cd6781b93030facfc703ebf61
Faputa <2238932729@qq.com>
2025-01-21 13:48:19 +0800

    正确实现嵌套查询

--- mybatis-ext/src/main/java/io/github/mybatisext/dialect/H2Dialect.java
+++ mybatis-ext/src/main/java/io/github/mybatisext/dialect/H2Dialect.java
@@ -191,7 +191,7 @@ public class H2Dialect extends BaseDialect {
             tables.add(joinTableInfo.getTableInfo() + " " + joinTableInfo.getAlias());
             joinTableInfo.getLeftJoinTableInfos().forEach((joinColumnInfo, leftJoinTableInfo) -> {
                 Condition condition = new Condition(ConditionType.BASIC);
-                condition.setExprTemplate(leftJoinTableInfo.getAlias() + "." + joinColumnInfo.getLeftColumn().getColumnName() + " = " + joinTableInfo.getAlias() + "." + joinColumnInfo.getRightColumn().getColumnName());
+                condition.setExprTemplate(joinTableInfo.getAlias() + "." + joinColumnInfo.getRightColumn().getColumnName() + " = " + leftJoinTableInfo.getAlias() + "." + joinColumnInfo.getLeftColumn().getColumnName());
                 conditions.add(condition);
             });
         }
