c63896c695725d01dce79faa4f499e21287a0eaa
Faputa <2238932729@qq.com>
2025-03-27 18:18:14 +0800

    使用javassist动态代理增强Configuration

--- mybatis-ext/src/test/java/io/github/mybatisext/jpa/ConditionHelperTest.java
+++ mybatis-ext/src/test/java/io/github/mybatisext/jpa/ConditionHelperTest.java
@@ -7,11 +7,12 @@ import java.util.Set;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.junit.jupiter.api.Test;
 
-import io.github.mybatisext.adapter.ExtConfiguration;
+import io.github.mybatisext.adapter.ConfigurationFactory;
 import io.github.mybatisext.adapter.ExtContext;
 import io.github.mybatisext.annotation.TableRef;
 import io.github.mybatisext.metadata.TableInfo;
@@ -31,7 +32,7 @@ public class ConditionHelperTest {
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
         ExtContext extContext = new ExtContext();
-        ExtConfiguration configuration = new ExtConfiguration(environment, extContext);
+        Configuration configuration = ConfigurationFactory.create(environment, extContext);
         TableInfoFactory tableInfoFactory = new TableInfoFactory(configuration, extContext);
         TableInfo tableInfo = tableInfoFactory.getTableInfo(TablePermission.class);
         Condition condition = ConditionHelper.fromTableInfo(tableInfo, false, "pt");
