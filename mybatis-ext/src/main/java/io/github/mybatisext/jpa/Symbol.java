9f1d69c36350710ee04a88ebc3e9efa2d4f129d7
Faputa <2238932729@qq.com>
2025-04-27 17:41:51 +0800

    state.getTokenizer()类型推断

--- mybatis-ext/src/main/java/io/github/mybatisext/jpa/Symbol.java
+++ mybatis-ext/src/main/java/io/github/mybatisext/jpa/Symbol.java
@@ -4,28 +4,29 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class Symbol {
+public class Symbol<T extends Tokenizer> {
 
     private final String name;
-    private final Set<Symbol> leftSymbols = new HashSet<>();
-    private Match match = (state, continuation) -> continuation.test(state);
+    private final Set<Symbol<T>> leftSymbols = new HashSet<>();
+    private Match<T> match = (state, continuation) -> continuation.test(state);
 
     public Symbol(String name) {
         this.name = name;
     }
 
-    public Symbol setMatch(Match match) {
+    public Symbol<T> setMatch(Match<T> match) {
         this.match = match;
         return this;
     }
 
-    public Symbol setLeftSymbols(Symbol... symbols) {
+    @SafeVarargs
+    public final Symbol<T> setLeftSymbols(Symbol<T>... symbols) {
         leftSymbols.clear();
         leftSymbols.addAll(Arrays.asList(symbols));
         return this;
     }
 
-    public Symbol set(Match match) {
+    public Symbol<T> set(Match<T> match) {
         this.match = (state, continuation) -> {
             Tokenizer tokenizer = state.getTokenizer();
             int begin = tokenizer.getCursor();
@@ -38,14 +39,14 @@ public final class Symbol {
         return this;
     }
 
-    public Symbol set(Symbol symbol) {
+    public Symbol<T> set(Symbol<T> symbol) {
         if (symbol.hasLeftRecursion(this)) {
             throw new ParserException("Left recursion detected in " + name);
         }
         this.match = (state, continuation) -> {
             Tokenizer tokenizer = state.getTokenizer();
             int begin = tokenizer.getCursor();
-            return symbol.match(new State(state, new Scope(name, state)), s2 -> {
+            return symbol.match(new State<>(state, new Scope<>(name, state)), s2 -> {
                 int end = tokenizer.getCursor();
                 Object value = s2.getReturn();
                 if (value == null) {
@@ -59,11 +60,11 @@ public final class Symbol {
         return setLeftSymbols(symbol);
     }
 
-    public boolean hasLeftRecursion(Symbol symbol) {
+    public boolean hasLeftRecursion(Symbol<T> symbol) {
         if (leftSymbols.contains(symbol)) {
             return true;
         }
-        for (Symbol leftSymbol : leftSymbols) {
+        for (Symbol<T> leftSymbol : leftSymbols) {
             if (leftSymbol.hasLeftRecursion(symbol)) {
                 return true;
             }
@@ -71,11 +72,11 @@ public final class Symbol {
         return false;
     }
 
-    public boolean match(Tokenizer tokenizer, Continuation continuation) {
-        return match(new State(tokenizer), continuation);
+    public boolean match(T tokenizer, Continuation<T> continuation) {
+        return match(new State<>(tokenizer), continuation);
     }
 
-    public boolean match(State state, Continuation continuation) {
+    public boolean match(State<T> state, Continuation<T> continuation) {
         return match.test(state, continuation);
     }
 
