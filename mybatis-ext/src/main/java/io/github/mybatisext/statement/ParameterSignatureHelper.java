914b23b213ce86f45f9dde50d87af8df641e2bb8
Faputa <2238932729@qq.com>
2025-05-19 23:21:52 +0800

    参考ParamNameResolver重构buildParameterSignature

--- mybatis-ext/src/main/java/io/github/mybatisext/statement/ParameterSignatureHelper.java
+++ mybatis-ext/src/main/java/io/github/mybatisext/statement/ParameterSignatureHelper.java
@@ -1,36 +1,110 @@
 package io.github.mybatisext.statement;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.reflection.ParamNameResolver;
 import org.apache.ibatis.reflection.ParamNameUtil;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
 import io.github.mybatisext.exception.MybatisExtException;
 import io.github.mybatisext.reflect.GenericMethod;
-import io.github.mybatisext.reflect.GenericParameter;
 import io.github.mybatisext.reflect.GenericType;
-import io.github.mybatisext.util.CommonUtils;
 
 public class ParameterSignatureHelper {
 
-    public static ParameterSignature buildParameterSignature(Configuration configuration, GenericMethod method) {
-        Map<String, GenericType> nameToType = buildNameToType(configuration, method);
-        if (nameToType.size() == 1) {
-            Map.Entry<String, GenericType> entry = nameToType.entrySet().iterator().next();
-            return buildSignatureIfCollection(entry.getValue().getType(), entry.getKey());
+    // org.apache.ibatis.reflection.ParamNameResolver
+    public static ParameterSignature buildParameterSignature(Configuration config, GenericMethod method) {
+        boolean useActualParamName = config.isUseActualParamName();
+        boolean hasParamAnnotation = false;
+        GenericType[] paramTypes = method.getGenericParameterTypes();
+        Annotation[][] paramAnnotations = method.getParameterAnnotations();
+        SortedMap<Integer, String> map = new TreeMap<>();
+        int paramCount = paramAnnotations.length;
+        for (int paramIndex = 0; paramIndex < paramCount; paramIndex++) {
+            if (isSpecialParameter(paramTypes[paramIndex].getType())) {
+                continue;
+            }
+            String name = null;
+            for (Annotation annotation : paramAnnotations[paramIndex]) {
+                if (annotation instanceof Param) {
+                    hasParamAnnotation = true;
+                    name = ((Param) annotation).value();
+                    break;
+                }
+            }
+            if (name == null) {
+                if (useActualParamName) {
+                    name = getActualParamName(method.getMethod(), paramIndex);
+                }
+                if (name == null) {
+                    name = String.valueOf(map.size());
+                }
+            }
+            map.put(paramIndex, name);
         }
+        SortedMap<Integer, String> names = Collections.unmodifiableSortedMap(map);
+        return getNamedParams(method.getGenericParameterTypes(), names, useActualParamName, hasParamAnnotation);
+    }
+
+    private static boolean isSpecialParameter(Class<?> clazz) {
+        return RowBounds.class.isAssignableFrom(clazz) || ResultHandler.class.isAssignableFrom(clazz);
+    }
+
+    public static ParameterSignature getNamedParams(GenericType[] parameterTypes, SortedMap<Integer, String> names, boolean useActualParamName, boolean hasParamAnnotation) {
+        int paramCount = names.size();
+        if (parameterTypes == null || paramCount == 0) {
+            return new ParameterSignature();
+        }
+        if (!hasParamAnnotation && paramCount == 1) {
+            GenericType genericType = parameterTypes[names.firstKey()];
+            return wrapToMapIfCollection(genericType.getType(), useActualParamName ? names.get(names.firstKey()) : null);
+        } else {
+            ParameterSignature parameterSignature = new ParameterSignature();
+            parameterSignature.setType(MapperMethod.ParamMap.class);
+            int i = 0;
+            for (Map.Entry<Integer, String> entry : names.entrySet()) {
+                parameterSignature.getNameToType().put(entry.getValue(), parameterTypes[entry.getKey()].getType());
+                String genericParamName = ParamNameResolver.GENERIC_NAME_PREFIX + (i + 1);
+                if (!names.containsValue(genericParamName)) {
+                    parameterSignature.getNameToType().put(genericParamName, parameterTypes[entry.getKey()].getType());
+                }
+                i++;
+            }
+            return parameterSignature;
+        }
+    }
+
+    public static ParameterSignature wrapToMapIfCollection(Class<?> object, String actualParamName) {
         ParameterSignature parameterSignature = new ParameterSignature();
-        parameterSignature.setType(MapperMethod.ParamMap.class);
-        for (Map.Entry<String, GenericType> entry : nameToType.entrySet()) {
-            parameterSignature.getNameToType().put(entry.getKey(), entry.getValue().getType());
+        if (Collection.class.isAssignableFrom(object)) {
+            parameterSignature.setType(MapperMethod.ParamMap.class);
+            parameterSignature.getNameToType().put("collection", object);
+            if (List.class.isAssignableFrom(object)) {
+                parameterSignature.getNameToType().put("list", object);
+            }
+            Optional.ofNullable(actualParamName).ifPresent(name -> parameterSignature.getNameToType().put(name, object));
+            return parameterSignature;
         }
+        if (object.isArray()) {
+            parameterSignature.setType(MapperMethod.ParamMap.class);
+            parameterSignature.getNameToType().put("array", object);
+            Optional.ofNullable(actualParamName).ifPresent(name -> parameterSignature.getNameToType().put(name, object));
+            return parameterSignature;
+        }
+        parameterSignature.setType(object);
         return parameterSignature;
     }
 
@@ -39,6 +113,9 @@ public class ParameterSignatureHelper {
     }
 
     public static boolean isParameterSignatureMatch(Object object, ParameterSignature parameterSignature) {
+        if (object == null) {
+            return parameterSignature.getType() == null;
+        }
         if (object instanceof MapperMethod.ParamMap) {
             MapperMethod.ParamMap<?> paramMap = (MapperMethod.ParamMap<?>) object;
             if (paramMap.size() != parameterSignature.getNameToType().size()) {
@@ -59,7 +136,7 @@ public class ParameterSignatureHelper {
     }
 
     public static String toString(ParameterSignature parameterSignature) {
-        String s = parameterSignature.getType().getName();
+        String s = parameterSignature.getType() != null ? parameterSignature.getType().getName() : "";
         List<String> ss = new ArrayList<>();
         for (Map.Entry<String, Class<?>> entry : parameterSignature.getNameToType().entrySet()) {
             ss.add(entry.getKey() + ":" + entry.getValue().getName());
@@ -92,56 +169,7 @@ public class ParameterSignatureHelper {
         }
     }
 
-    private static Map<String, GenericType> buildNameToType(Configuration configuration, GenericMethod method) {
-        GenericParameter[] parameters = method.getParameters();
-        Map<String, GenericType> nameToType = new HashMap<>();
-        for (int i = 0; i < parameters.length; i++) {
-            if (CommonUtils.isSpecialParameter(parameters[i].getType())) {
-                continue;
-            }
-            String name = null;
-            Param param = parameters[i].getAnnotation(Param.class);
-            if (param != null) {
-                name = param.value();
-            } else {
-                if (configuration.isUseActualParamName()) {
-                    name = getActualParamName(method.getMethod(), i);
-                }
-                if (name == null) {
-                    name = String.valueOf(nameToType.size());
-                }
-            }
-            nameToType.put(name, parameters[i].getGenericType());
-        }
-        return nameToType;
-    }
-
     private static String getActualParamName(Method method, int paramIndex) {
         return ParamNameUtil.getParamNames(method).get(paramIndex);
-    }
-
-    private static ParameterSignature buildSignatureIfCollection(Class<?> type, String actualParamName) {
-        ParameterSignature parameterSignature = new ParameterSignature();
-        if (Collection.class.isAssignableFrom(type)) {
-            parameterSignature.setType(MapperMethod.ParamMap.class);
-            parameterSignature.getNameToType().put("collection", type);
-            if (List.class.isAssignableFrom(type)) {
-                parameterSignature.getNameToType().put("list", type);
-            }
-            if (actualParamName != null) {
-                parameterSignature.getNameToType().put(actualParamName, type);
-            }
-            return parameterSignature;
-        }
-        if (type.isArray()) {
-            parameterSignature.setType(MapperMethod.ParamMap.class);
-            parameterSignature.getNameToType().put("array", type);
-            if (actualParamName != null) {
-                parameterSignature.getNameToType().put(actualParamName, type);
-            }
-            return parameterSignature;
-        }
-        parameterSignature.setType(type);
-        return parameterSignature;
     }
 }
